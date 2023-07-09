a01fad97cc146fdb37a8816eb8a247fc255da8c8
aparvate <amogh.parvate@gmail.com>
2023-07-09 13:51:24 -0700

    added important files; new console log

--- background.js
+++ background.js
@@ -1,7 +1,6 @@
-// // Event listener to check if on tab
-// chrome.tabs.onUpdated.addListener((tabId, tab) => {
-//     if (tab.url && tab.url.includes("")) {
-//         const queryParameters = tab.url.split("?")[1];
-//         const urlParameters = new URLSearchParams(queryParameters);
-//     }
-// });
+// Event listener to check if on tab
+chrome.tabs.onUpdated.addListener((tabId, tab) => {
+    if (tab.url && tab.url.includes("enroll.wisc.edu")) {
+        console.log("Correct tab detected!");
+    }
+});
