8c44758e66e03fe8cc9ef484a1b4a95f1c8bdd11
BaileyKau <88602557+BaileyKau@users.noreply.github.com>
2023-07-12 12:47:43 -0700

    Co-authored-by: aparvate <aparvate@users.noreply.github.com> Co-authored-by: matthew.wang7@outlook.com <matthew.wang7@outlook.com>

--- contentScript.js
+++ contentScript.js
@@ -2,7 +2,16 @@
         //Getting the course title
         var courseTitle = document.getElementsByClassName("course-title")[0];
         if (courseTitle) { courseTitle = courseTitle.innerHTML; }
+
         //Madgrades API 
+        fetch("https://api.madgrades.com/v1/courses?" + new URLSearchParams({ query: courseTitle }).toString, {
+            method: 'GET',
+            headers: {
+                'Authorization' : "Token token=6cd3473136464c3d854e2f7c5372807c",
+            }
+        })
+            .then(response => response.json())
+            .then(json => console.log(json))
 
         //Getting the professors
 
@@ -10,7 +19,7 @@
     }
     
     chrome.runtime.onMessage.addListener((obj, sender, response) => {
-        console.log("Message received! Yippee!");
+        // console.log("Message received! Yippee!");
         const { message } = obj;
         if (message === 'NEW'){
             courseSelected();
