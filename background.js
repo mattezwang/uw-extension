222fef83b763162a9e70b4649e27c261abac3cbe
BaileyKau <88602557+BaileyKau@users.noreply.github.com>
2023-07-18 13:18:47 -0700

    Co-authored-by: Matthew Wang <mattezwang@users.noreply.github.com> Co-authored-by: aparvate <aparvate@users.noreply.github.com>

--- background.js
+++ background.js
@@ -1,12 +1,12 @@
-// Event listener to check if on tab
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (tab.url && tab.url.includes('enroll.wisc.edu')) {
-        console.log('Clicked on Tab! ' + tab.url);
+// // Event listener to check if on tab
+// chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+//     if (tab.url && tab.url.includes('enroll.wisc.edu')) {
+//         console.log('Clicked on Tab! ' + tab.url);
         
-        chrome.tabs.sendMessage(tabId, {
-            message: 'NEW'
-        });
-        console.log("Message sent!");
+//         chrome.tabs.sendMessage(tabId, {
+//             message: 'NEW'
+//         });
+//         console.log("Message sent!");
     
-    }
-});
+//     }
+// });
--- contentScript.js
+++ contentScript.js
@@ -1,27 +1,57 @@
-    const courseSelected = () => {
-        //Getting the course title
-        var courseTitle = document.getElementsByClassName("course-title")[0];
-        if (courseTitle) { courseTitle = courseTitle.innerHTML; }
+var buttons = document.getElementsByClassName("ng-star-inserted");
+var instructors = document.getElementsByClassName("instructor ng-star-inserted");
+var prevCourseNum = null;
+var counter = 0;
 
-        //Madgrades API 
-        fetch("https://api.madgrades.com/v1/courses?" + new URLSearchParams({ query: courseTitle }).toString, {
-            method: 'GET',
-            headers: {
-                'Authorization' : "Token token=6cd3473136464c3d854e2f7c5372807c",
-            }
-        })
-            .then(response => response.json())
-            .then(json => console.log(json))
+for (let i = 0; i < buttons.length; i++) {
+
+// CAN ADD IF STATEMENT FOR IF THE USER IS PRESSING THE BACK BUTTON
+// ENAHNCE / OPTIMAIZE THIS FOR LOOP
+
+    buttons[i].addEventListener("click", () => { courseSelected(); professorRating();});
+    
+}
+
+    const courseSelected = () => {
+        var courseNum = document.getElementsByClassName("catalog-ref")[0]
+        if (courseNum != prevCourseNum) {
+            //Getting the course title
+            var courseTitle = document.getElementsByClassName("course-title")[0];
+            var catalogRef = document.getElementsByClassName("catalog-ref")[0];
+            var url;
+            if (courseTitle) { courseTitle = courseTitle.innerHTML; }
+            if (catalogRef) { catalogRef = catalogRef.innerHTML; }
+            var search = courseTitle + " " + catalogRef;
+            counter = 0;
+            prevCourseNum = courseNum;
+
+
+            // WHEN YOU BACK OUT OF THE PAGE, IT OUTPUTS THE FETCHED DATA TWICE
+            //Madgrades API 
+            fetch("https://api.madgrades.com/v1/courses?" + new URLSearchParams({ query: search }).toString(), {
+                method: 'GET',
+                headers: {
+                    'Authorization' : "Token token=6cd3473136464c3d854e2f7c5372807c",
+                }
+            }).then(response => response.json()).then(json => console.log(json))
+        }
 
         //Getting the professors
-
+            
         //Rate My Professor
     }
-    
-    chrome.runtime.onMessage.addListener((obj, sender, response) => {
-        // console.log("Message received! Yippee!");
-        const { message } = obj;
-        if (message === 'NEW'){
-            courseSelected();
+
+    const professorRating = () => {
+        for (let element of instructors) {
+            if (counter < instructors.length){element.innerHTML = element.innerHTML + "<p>Hi...</p>"; counter++;}
+            
         }
-    });
+    }
+    
+    // chrome.runtime.onMessage.addListener((obj, sender, response) => {
+    //     // console.log("Message received! Yippee!");
+    //     const { message } = obj;
+    //     if (message === 'NEW'){
+    //         courseSelected();
+    //     }
+    // });
