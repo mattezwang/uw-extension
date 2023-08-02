2e8130ea8dda0bad98bd3564818491235129b9d9
aparvate <amogh.parvate@gmail.com>
2023-08-02 12:39:56 -0700

    Bugfix, professor now shows up well

--- contentScript.js
+++ contentScript.js
@@ -2,14 +2,19 @@ var buttons = document.getElementsByClassName("ng-star-inserted");
 var instructors = document.getElementsByClassName("instructor ng-star-inserted");
 var instructorPage = document.getElementsByClassName("ng-tns-c164-1 ng-star-inserted");
 var styleHead = document.head;
+var mainDoc = document.querySelector("mat-sidenav-container");
 var prevCourseNum = null;
+var counter = 0;
+var labelPresent = false;
 
-for (let i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener("click", () => { courseSelected(); professorRating();});
+function buttonListener () {
+    mainDoc.addEventListener("click", () => {
+        courseSelected();
+        professorRating();
+    });
 }
 
-<<<<<<< HEAD
-const courseSelected = () => {
+function courseSelected () {
     var courseNum = document.getElementsByClassName("catalog-ref")[0]
     if (courseNum != prevCourseNum) {
         //Getting the course title
@@ -20,31 +25,20 @@ const courseSelected = () => {
         prevCourseNum = courseNum;
     }
 }
-=======
-    const courseSelected = () => {
-        var courseNum = document.getElementsByClassName("catalog-ref")[0]
-        if (courseNum != prevCourseNum) {
-            //Getting the course title
-            var courseTitle = document.getElementsByClassName("course-title")[0];
-            var catalogRef = document.getElementsByClassName("catalog-ref")[0];
-            if (courseTitle) { courseTitle = courseTitle.innerHTML; }
-            if (catalogRef) { catalogRef = catalogRef.innerHTML; }
-            var search = courseTitle + " " + catalogRef;
-            counter = 0;
-            prevCourseNum = courseNum;
+
+    //Getting the professors
+    const professorRating = () => {
+        for (let element of instructors) {
+            console.log(counter + " counter, instructors.length = " + instructors.length);
+            if (counter < instructors.length){element.innerHTML = element.innerHTML + "<p style=\"background-color: #6bcc5c; border: 5px solid #a7fc9a; border-radius: 10px; color: #c6f5d3; max-width: 50px; padding: 7px 10px; text-align: center;\">★: 4.3</p>"; counter++;}
         }
     }
 
->>>>>>> 5041744f97accabf51a346a2a28cdae8eb381820
-    //Getting the professors
-    const professorRating = () => {
-        var counter = 0;
-        for (let element of instructors) {
-            console.log(counter + " counter");
-<<<<<<< HEAD
-            if (counter < instructors.length){element.innerHTML.match(/> (.*?) </gi); element.innerHTML = element.innerHTML + "<p style=\"background-color: #6bcc5c; border: 5px solid #a7fc9a; border-radius: 10px; color: #c6f5d3; max-width: 50px; padding: 7px 10px; text-align: center;\">★: 4.3</p>"; counter++;}
-=======
-            if (counter < instructors.length){alert(element.innerHTML.match(/> (.*?) </gi)); element.innerHTML = element.innerHTML + "<p style=\"background-color: #6bcc5c; border: 5px solid #a7fc9a; border-radius: 10px; color: #c6f5d3; max-width: 50px; padding: 7px 10px; text-align: center;\">★: 4.3</p>"; counter++;}
->>>>>>> 5041744f97accabf51a346a2a28cdae8eb381820
+    const observer = new MutationObserver((mutations) => {
+        if (mutations[0].attributeName === 'class') {
+          console.log('class change seen');
+          counter = 0;
         }
-    }
+        buttonListener();
+      })
+        .observe(mainDoc, { attributes: true });
