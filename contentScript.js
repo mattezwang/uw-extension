6086a6ac54e193c449e5ab9e7f7d0fe8dba948a2
BaileyKau <88602557+BaileyKau@users.noreply.github.com>
2023-08-02 12:43:33 -0700

    Co-authored-by: matthew.wang7@outlook.com <matthew.wang7@outlook.com> Co-authored-by: aparvate <aparvate@users.noreply.github.com>

--- contentScript.js
+++ contentScript.js
@@ -8,19 +8,6 @@ for (let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener("click", () => { courseSelected(); professorRating();});
 }
 
-<<<<<<< HEAD
-const courseSelected = () => {
-    var courseNum = document.getElementsByClassName("catalog-ref")[0]
-    if (courseNum != prevCourseNum) {
-        //Getting the course title
-        var courseTitle = document.getElementsByClassName("course-title")[0];
-        var catalogRef = document.getElementsByClassName("catalog-ref")[0];
-        if (courseTitle) { courseTitle = courseTitle.innerHTML; }
-        if (catalogRef) { catalogRef = catalogRef.innerHTML; }
-        prevCourseNum = courseNum;
-    }
-}
-=======
     const courseSelected = () => {
         var courseNum = document.getElementsByClassName("catalog-ref")[0]
         if (courseNum != prevCourseNum) {
@@ -35,16 +22,11 @@ const courseSelected = () => {
         }
     }
 
->>>>>>> 5041744f97accabf51a346a2a28cdae8eb381820
     //Getting the professors
     const professorRating = () => {
         var counter = 0;
         for (let element of instructors) {
             console.log(counter + " counter");
-<<<<<<< HEAD
             if (counter < instructors.length){element.innerHTML.match(/> (.*?) </gi); element.innerHTML = element.innerHTML + "<p style=\"background-color: #6bcc5c; border: 5px solid #a7fc9a; border-radius: 10px; color: #c6f5d3; max-width: 50px; padding: 7px 10px; text-align: center;\">★: 4.3</p>"; counter++;}
-=======
-            if (counter < instructors.length){alert(element.innerHTML.match(/> (.*?) </gi)); element.innerHTML = element.innerHTML + "<p style=\"background-color: #6bcc5c; border: 5px solid #a7fc9a; border-radius: 10px; color: #c6f5d3; max-width: 50px; padding: 7px 10px; text-align: center;\">★: 4.3</p>"; counter++;}
->>>>>>> 5041744f97accabf51a346a2a28cdae8eb381820
         }
     }
