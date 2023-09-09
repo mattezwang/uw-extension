69b7b56571d90019922ffd17d9358d31b68d6136
aparvate <amogh.parvate@gmail.com>
2023-09-09 15:51:40 -0500

    Updated eel, using Python 3.8 now, working on it

--- background.py
+++ background.py
@@ -1,7 +1,9 @@
-import MySQLdb
-from flask import Flask, jsonify, render_template
+import pymysql
+import eel
+eel.init('templates')
+eel.start('index.html', port=43935)
 
-connection = MySQLdb.connect(
+connection = pymysql.connect(
     host="gateway01.us-west-2.prod.aws.tidbcloud.com",
     port=4000,
     user="42yppRwczsvpiU9.root",
@@ -13,23 +15,10 @@ connection = MySQLdb.connect(
       }
     )
 
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-  return render_template('index.html')
-
-@app.route('/accessData')
-def samplefunction():
-    #access your DB get your results here
-    courseName = "Programming II"
-
-    with connection:
-      with connection.cursor() as cursor:
-        cursor.execute("SELECT MAX(((a_count * 4) + (ab_count * 3.5) + (b_count * 3) + (bc_count * 2.5) + (c_count * 2) + (d_count * 1) + (f_count * 0))/(a_count + ab_count + b_count + bc_count + c_count + d_count + f_count)) FROM `grade_distributions` JOIN `course_offerings` ON `grade_distributions`.`course_offering_uuid` = `course_offerings`.`uuid` WHERE course_offerings.name = \'" + courseName + "\'")
-        m = cursor.fetchone()
-    return jsonify(m)
-    
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=43935)
+@eel.expose
+def getGrades(a):
+  courseName = a
+  with connection:
+    with connection.cursor() as cursor:
+      cursor.execute("SELECT MAX(((a_count * 4) + (ab_count * 3.5) + (b_count * 3) + (bc_count * 2.5) + (c_count * 2) + (d_count * 1) + (f_count * 0))/(a_count + ab_count + b_count + bc_count + c_count + d_count + f_count)) FROM `grade_distributions` JOIN `course_offerings` ON `grade_distributions`.`course_offering_uuid` = `course_offerings`.`uuid` WHERE course_offerings.name = \'" + courseName + "\'")
+      m = cursor.fetchone()
